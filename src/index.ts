3f781af7bd6b11e919831c335688936478bdaff1
gituser <gituser@synds.nadaniente>
2020-10-23 01:03:46 +0200

    Added SSN checksum calculation and validation.

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,111 @@ function ssnIsValid(value) {
     }
   }
 
-  return !isNaN( Date.parse(dateStr) ) && expression.test(value.replace(/\W/g, ''));
+  return !isNaN( Date.parse(dateStr) ) && expression.test(value.replace(/\W/g, '')) && hasValidSsnChecksum(value) ;
+}
+
+
+/**
+ * Calculate checksum function.
+ */
+function ssnCalculateChecksum(numberToCalculateChecksumFor: number): number {
+  let numberAsString: string;
+  let checksum: number;
+
+  // Just get the digits as a String
+  numberAsString = String(numberToCalculateChecksumFor)
+  
+  // Initialize the sum
+  checksum = 0;
+
+  switch(numberAsString.length) {
+      case 8:
+        // The leading "0" got lost so add it
+        numberAsString = "0" + numberAsString;
+      break;
+      case 9:
+        // It already has the correct length
+        break;
+      case 10:
+        // Cut off the last digit as this might be the checksum
+        numberAsString = `${numberAsString.substring(0, 9)}`;
+        break;
+      case 11:
+        // Cut off the first two digits (and ignore the nonexisting checksum that somebody already might have removed it), so the same steps as for case "12:" below
+      case 12:
+        // Cut off the first two and the last digits as they are not used
+        numberAsString = `${numberAsString.substring(2, 11)}`;
+        break;
+      default:
+        // The format is not valid --> Throw an error
+        throw new Error('Invalid Swedish Social Security Number');
+        break;
+    }
+    
+    // Calculate the checksum
+    // First iterate over the digits and multiply them with 2, 1, 2, 1, 2, 1, 2, 1, 2
+    for (let i = 0; i < numberAsString.length; i++) {
+      let currentNumber = parseInt(numberAsString.charAt(i));
+      // Every second digit needs to be multiplied with two
+      if (i % 2 === 0) {
+        currentNumber = currentNumber * 2;
+      }
+      // Make sure to handle the new two digit numbers, that might occur, correctly
+      if (currentNumber > 9) {
+        currentNumber = currentNumber - 9;
+      }
+      // Add the number to the temporary checksum value 
+      checksum = checksum + currentNumber;
+  }
+  // Apply the last calculation steps
+  // Get the last digit
+  checksum = checksum % 10;
+  
+  // Check if it is zero and if yes use 0 as this is mentioned in the definition
+  if (checksum === 0) {
+    checksum = 0;
+  } else {
+    // Calculate 10 - last digit
+    checksum = 10 - checksum;
+  }
+  return checksum;
+}
+
+/**
+ * Returns only the digits as a number.
+ */
+function ssnExtractDigitsAsNumber(ssnToExtractDigitsFrom: string): number {
+  let digitsAsString: string;
+  let extractedDigitsAsNumber: number;
+
+  // Just get the digits as a String
+  digitsAsString = ssnToExtractDigitsFrom.replace(/\W/g, '');
+  
+  extractedDigitsAsNumber = Number(digitsAsString)
+  
+  return extractedDigitsAsNumber;
+}
+
+/**
+ * Checks if the given SSN has the correct checksum.
+ */
+function hasValidSsnChecksum(ssnToCheckChecksumFor: string): boolean {
+  let digitsAsString: string;
+  let extractedChecksum: number;
+  let calculatedChecksum: number;
+  let extractedDigitsAsNumber: number;
+
+  // Get the digits as a number
+  extractedDigitsAsNumber = ssnExtractDigitsAsNumber(ssnToCheckChecksumFor);
+  // Get the digits as a String
+  digitsAsString = String(extractedDigitsAsNumber);
+  // Extract the checksum from the string
+  extractedChecksum = Number(digitsAsString.substring(digitsAsString.length-1, digitsAsString.length));
+  
+  // Calculate the checksum
+  calculatedChecksum = ssnCalculateChecksum(extractedDigitsAsNumber);
+  // Return the result of the comparison between calculated and given checksum
+  return calculatedChecksum === extractedChecksum;
 }
 
 /**
